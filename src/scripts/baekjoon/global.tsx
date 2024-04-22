97986175308986be1331ecb533f62941fab7e39c
양유경 <gosjorb@gmail.com>
2024-04-22 14:26:08 +0900

    Refactor: addSolveButton 함수로 감쌈

--- src/scripts/baekjoon/global.tsx
+++ src/scripts/baekjoon/global.tsx
@@ -4,26 +4,30 @@ import SolveButton from "./solve/components/SolveButton";
 import { getProblemId } from "./utils";
 
 const customGlobalPage = (): void => {
-  const submitButton = document.querySelector(
-    'ul.problem-menu li a[href*="/submit"]'
-  );
+  const addSolveButton = () => {
+    const submitButton = document.querySelector(
+      'ul.problem-menu li a[href*="/submit"]'
+    );
 
-  if (submitButton) {
-    const submitListItem = submitButton.closest("li");
+    if (submitButton) {
+      const submitListItem = submitButton.closest("li");
 
-    if (submitListItem) {
-      const problemId: string | null = getProblemId();
-      const listItem = document.createElement("li");
+      if (submitListItem) {
+        const problemId: string | null = getProblemId();
+        const listItem = document.createElement("li");
 
-      submitListItem.parentNode?.insertBefore(
-        listItem,
-        submitListItem.nextSibling
-      );
+        submitListItem.parentNode?.insertBefore(
+          listItem,
+          submitListItem.nextSibling
+        );
 
-      const root = createRoot(listItem);
-      root.render(<SolveButton problemId={problemId} />);
+        const root = createRoot(listItem);
+        root.render(<SolveButton problemId={problemId} />);
+      }
     }
-  }
+  };
+
+  addSolveButton();
 };
 
 export default customGlobalPage;
