07c25dc5a44d293bb94f317f26aea44df8e9bd4d
paul <cebu13@gmail.com>
2024-04-23 17:26:27 +0900

    Fix: 노트 모달 꺼지는 오류 해결

--- src/scripts/baekjoon/note/components/Modal/Modal.tsx
+++ src/scripts/baekjoon/note/components/Modal/Modal.tsx
@@ -7,46 +7,45 @@ const Modal = () => {
         if (modalBackdrop) {
             modalBackdrop.remove();
         }
+        console.log('close modal');
     };
     const save = () => {
         console.log('save');
     };
 
     return (
-        <div className='modal-backdrop' onClick={closeModal}>
-            <div className='modal-content'>
-                <div className='modal-header' style={{ display: 'flex' }}>
-                    <h5 className='modal-title'>오답 노트 작성</h5>
-                    <button
-                        type='button'
-                        className='close'
-                        data-dismiss='modal'
-                        aria-label='Close'
-                        onClick={closeModal}
-                    >
-                        <span aria-hidden='true'>&times;</span>
-                    </button>
-                </div>
-                <div className='modal-body'>
-                    <textarea rows={6} cols={50}></textarea>
-                </div>
-                <div className='modal-footer'>
-                    <button
-                        type='button'
-                        className='btn btn-secondary'
-                        data-dismiss='modal'
-                        onClick={closeModal}
-                    >
-                        취소
-                    </button>
-                    <button
-                        type='button'
-                        className='btn btn-primary'
-                        onClick={save}
-                    >
-                        저장
-                    </button>
-                </div>
+        <div className='modal-content'>
+            <div className='modal-header' style={{ display: 'flex' }}>
+                <h5 className='modal-title'>오답 노트 작성</h5>
+                <button
+                    type='button'
+                    className='close'
+                    data-dismiss='modal'
+                    aria-label='Close'
+                    onClick={closeModal}
+                >
+                    <span aria-hidden='true'>&times;</span>
+                </button>
+            </div>
+            <div className='modal-body'>
+                <textarea rows={6} cols={50}></textarea>
+            </div>
+            <div className='modal-footer'>
+                <button
+                    type='button'
+                    className='btn btn-secondary'
+                    data-dismiss='modal'
+                    onClick={closeModal}
+                >
+                    취소
+                </button>
+                <button
+                    type='button'
+                    className='btn btn-primary'
+                    onClick={save}
+                >
+                    저장
+                </button>
             </div>
         </div>
     );
