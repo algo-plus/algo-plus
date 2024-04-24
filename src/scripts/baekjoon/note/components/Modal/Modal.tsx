bafa9f0dab64ea81d97e09196f8ee0f599d000c7
paul <cebu13@gmail.com>
2024-04-24 15:03:06 +0900

    Feat: code comparison layout

--- src/scripts/baekjoon/note/components/Modal/Modal.tsx
+++ src/scripts/baekjoon/note/components/Modal/Modal.tsx
@@ -15,50 +15,161 @@ const Modal = () => {
         console.log('save');
     };
 
-    interface CodeDiffProps {
-        oldCode: string;
-        newCode: string;
-    }
+    // const CodeDiff: React.FC<CodeDiffProps> = ({ oldCode, newCode }) => {
+    //     const generateDiff = () => {
+    //         const diff = Diff.diffLines(oldCode, newCode);
+    //         return diff.map((part, index) => {
+    //             if (part.added) {
+    //                 return (
+    //                     <span
+    //                         key={index}
+    //                         style={{ backgroundColor: 'lightgreen' }}
+    //                     >
+    //                         + {part.value}
+    //                     </span>
+    //                 );
+    //             }
+    //             if (part.removed) {
+    //                 return (
+    //                     <span
+    //                         key={index}
+    //                         style={{ backgroundColor: 'lightcoral' }}
+    //                     >
+    //                         - {part.value}
+    //                     </span>
+    //                 );
+    //             }
+    //             return <span key={index}>{part.value}</span>;
+    //         });
+    //     };
 
-    const CodeDiff: React.FC<CodeDiffProps> = ({ oldCode, newCode }) => {
-        const generateDiff = () => {
-            const diff = Diff.diffLines(oldCode, newCode);
-            return diff.map((part, index) => {
-                if (part.added) {
-                    return (
-                        <span
-                            key={index}
-                            style={{ backgroundColor: 'lightgreen' }}
-                        >
-                            + {part.value}
-                        </span>
-                    );
+    //     return <div>{generateDiff()}</div>;
+    // };
+
+    const oldCode: string = `import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    
+    public class Main {
+        private static int N, M;
+        private static char[][] graph;
+        private static boolean[][] visited;
+        private static int ans = Integer.MIN_VALUE;
+        private static int[] dx = { 0, 1, 0, -1 };
+        private static int[] dy = { 1, 0, -1, 0 };
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            N = read();
+            M = read();
+            graph = new char[N][M];
+            visited = new boolean[N][M];
+    
+            String line;
+            for (int i = 0; i < N; i++) {
+                line = br.readLine();
+                for (int j = 0; j < M; j++) {
+                    graph[i][j] = line.charAt(j);
                 }
-                if (part.removed) {
-                    return (
-                        <span
-                            key={index}
-                            style={{ backgroundColor: 'lightcoral' }}
-                        >
-                            - {part.value}
-                        </span>
-                    );
-                }
-                return <span key={index}>{part.value}</span>;
-            });
-        };
+            }
+            dfs(0, 0, 1);
+            System.out.println(ans);
+        }
+    
+        private static void dfs(int r, int c, int cnt) {
+            if (visited[r][c]) {
+                System.out.println(-1);
+                System.exit(0);
+            }
+            visited[r][c] = true;
+            int nr, nc, len;
+            for (int i = 0; i < 4; i++) {
+                len = graph[r][c] - '0';
+                nr = r + len * dx[i];
+                nc = c + len * dy[i];
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M || graph[nr][nc] == 'H')
+                    continue;
+                dfs(nr, nc, cnt + 1);
+                visited[nr][nc] = false;
+            }
+            ans = Math.max(ans, cnt);
+            return;
+        }
+        static int read() throws IOException {
+            int c, n = System.in.read() & 15;
+            while ((c = System.in.read()) > 32)
+                n = (n << 3) + (n << 1) + (c & 15);
+            if (c == 13)
+                System.in.read();
+            return n;
+        }
+    }`;
 
-        return <div>{generateDiff()}</div>;
-    };
+    const newCode: string = `import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.Arrays;
+    
+    public class Main {
+        private static int N, M;
+        private static char[][] graph;
+        private static int[][] dp;
+        private static boolean[][] visited;
+        private static int tmp;
+        private static int[] dx = { 0, 1, 0, -1 };
+        private static int[] dy = { 1, 0, -1, 0 };
+    
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            N = read();
+            M = read();
+            graph = new char[N][M];
+            dp = new int[N][M];
+            visited = new boolean[N][M];
+    
+            String line;
+            for (int i = 0; i < N; i++) {
+                line = br.readLine();
+                Arrays.fill(dp[i], -1);
+                graph[i] = line.toCharArray();
+            }
+            System.out.println(dfs(0, 0) + 1);
+        }
+    
+        private static int dfs(int r, int c) {
+            if (visited[r][c]) {
+                System.out.println(-1);
+                System.exit(0);
+            }
+            if (dp[r][c] > -1) {
+                return dp[r][c];
+            }
+            visited[r][c] = true;
+            dp[r][c] = 0;
+            int nr, nc, len;
+            for (int i = 0; i < 4; i++) {
+                len = graph[r][c] - '0';
+                nr = r + len * dx[i];
+                nc = c + len * dy[i];
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M || graph[nr][nc] == 'H')
+                    continue;
+                tmp = dfs(nr, nc) + 1;
+                dp[r][c] = tmp > dp[r][c] ? tmp : dp[r][c];
+                visited[nr][nc] = false;
+            }
+            return dp[r][c];
+        }
+    
+        static int read() throws IOException {
+            int c, n = System.in.read() & 15;
+            while ((c = System.in.read()) > 32)
+                n = (n << 3) + (n << 1) + (c & 15);
+            if (c == 13)
+                System.in.read();
+            return n;
+        }
+    }`;
 
-    const oldCode: string = `function greet(name: string) {
-        console.log('Hello, ' + name + '!');
-      }`;
-
-    const newCode: string = `function greet(name: string) {
-        console.log('Hi, ' + name + '!');
-      }`;
-
+    // TODO: Adjust margin and padding of ReactDiffViewer
     return (
         <div className='modal-content'>
             <div className='modal-header' style={{ display: 'flex' }}>
@@ -74,11 +185,11 @@ const Modal = () => {
                 </button>
             </div>
             <div className='modal-body'>
-                <div>
+                <div className='codediff-container'>
                     <h6>코드 비교 결과:</h6>
-                    <pre>
+                    {/* <pre>
                         <CodeDiff oldCode={oldCode} newCode={newCode} />
-                    </pre>
+                    </pre> */}
                     <ReactDiffViewer
                         oldValue={oldCode}
                         newValue={newCode}
@@ -87,6 +198,7 @@ const Modal = () => {
                     />
                 </div>
                 <div>
+                    <h6>전체 코멘트</h6>
                     <textarea rows={6} cols={50}></textarea>
                 </div>
             </div>
