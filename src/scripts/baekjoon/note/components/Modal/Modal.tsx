ae3cf80f9dc5205de75c46d4781cde80039722e5
paul <cebu13@gmail.com>
2024-04-24 11:21:06 +0900

    Feat: code comparison layout

--- src/scripts/baekjoon/note/components/Modal/Modal.tsx
+++ src/scripts/baekjoon/note/components/Modal/Modal.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as Diff from 'diff';
+import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer-continued';
 import './Modal.css';
 
 const Modal = () => {
@@ -12,6 +14,50 @@ const Modal = () => {
     const save = () => {
         console.log('save');
     };
+
+    interface CodeDiffProps {
+        oldCode: string;
+        newCode: string;
+    }
+
+    const CodeDiff: React.FC<CodeDiffProps> = ({ oldCode, newCode }) => {
+        const generateDiff = () => {
+            const diff = Diff.diffLines(oldCode, newCode);
+            return diff.map((part, index) => {
+                if (part.added) {
+                    return (
+                        <span
+                            key={index}
+                            style={{ backgroundColor: 'lightgreen' }}
+                        >
+                            + {part.value}
+                        </span>
+                    );
+                }
+                if (part.removed) {
+                    return (
+                        <span
+                            key={index}
+                            style={{ backgroundColor: 'lightcoral' }}
+                        >
+                            - {part.value}
+                        </span>
+                    );
+                }
+                return <span key={index}>{part.value}</span>;
+            });
+        };
+
+        return <div>{generateDiff()}</div>;
+    };
+
+    const oldCode: string = `function greet(name: string) {
+        console.log('Hello, ' + name + '!');
+      }`;
+
+    const newCode: string = `function greet(name: string) {
+        console.log('Hi, ' + name + '!');
+      }`;
 
     return (
         <div className='modal-content'>
@@ -28,9 +74,21 @@ const Modal = () => {
                 </button>
             </div>
             <div className='modal-body'>
-                
-
-                <textarea rows={6} cols={50}></textarea>
+                <div>
+                    <h6>코드 비교 결과:</h6>
+                    <pre>
+                        <CodeDiff oldCode={oldCode} newCode={newCode} />
+                    </pre>
+                    <ReactDiffViewer
+                        oldValue={oldCode}
+                        newValue={newCode}
+                        compareMethod={DiffMethod.LINES}
+                        splitView={true}
+                    />
+                </div>
+                <div>
+                    <textarea rows={6} cols={50}></textarea>
+                </div>
             </div>
             <div className='modal-footer'>
                 <button
