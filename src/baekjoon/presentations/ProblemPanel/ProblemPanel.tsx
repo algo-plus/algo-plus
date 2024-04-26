a404c1aae45cccfad291d5d347b67fbd1cba766e
gusckd96 <gusckd96@naver.com>
2024-04-26 10:30:33 +0900

    Refactor: 백준 문제 가져오기 API 분리

--- src/baekjoon/presentations/ProblemPanel/ProblemPanel.tsx
+++ src/baekjoon/presentations/ProblemPanel/ProblemPanel.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './ProblemPanel.css';
+import { fetchProblemHtml } from '@/baekjoon/apis/problem';
 
 const ProblemPanel: React.FC<{ problemId: string | null }> = ({
     problemId,
@@ -9,50 +10,56 @@ const ProblemPanel: React.FC<{ problemId: string | null }> = ({
     );
 
     useEffect(() => {
-        const fetchProblemDetail = async () => {
-            try {
-                const response = await fetch(
-                    `https://www.acmicpc.net/problem/${problemId}`
-                );
-                const html = await response.text();
-                const doc = new DOMParser().parseFromString(html, 'text/html');
-                const problemContainer = doc.querySelector(
-                    '.container.content .row'
-                ) as HTMLElement;
-
-                if (problemContainer) {
-                    // 문제 메뉴, 즐겨찾기 버튼, 알고리즘 분류, 메모 제거
-                    const elementsToRemove = [
-                        'ul.problem-menu',
-                        '.problem-button',
-                        '#problem_tags',
-                        '#problem_memo',
-                    ];
-                    elementsToRemove.forEach((selector) => {
-                        const elem = problemContainer.querySelector(selector);
-                        if (elem && elem.parentNode) {
-                            elem.parentNode.removeChild(elem);
-                        }
-                    });
-
-                    problemContainer.style.margin = '0';
-
-                    const content = (
-                        <div
-                            className='problem-content'
-                            dangerouslySetInnerHTML={{
-                                __html: problemContainer.innerHTML,
-                            }}
-                        ></div>
+        const fetchProblemDetail = () => {
+            fetchProblemHtml(
+                problemId,
+                (html) => {
+                    const doc = new DOMParser().parseFromString(
+                        html,
+                        'text/html'
                     );
-                    setProblemContent(content);
-                } else {
+                    const problemContainer = doc.querySelector(
+                        '.container.content .row'
+                    ) as HTMLElement;
+
+                    if (problemContainer) {
+                        // 문제 메뉴, 즐겨찾기 버튼, 알고리즘 분류, 메모 제거
+                        const elementsToRemove = [
+                            'ul.problem-menu',
+                            '.problem-button',
+                            '#problem_tags',
+                            '#problem_memo',
+                        ];
+                        elementsToRemove.forEach((selector) => {
+                            const elem =
+                                problemContainer.querySelector(selector);
+                            if (elem && elem.parentNode) {
+                                elem.parentNode.removeChild(elem);
+                            }
+                        });
+
+                        problemContainer.style.margin = '0';
+
+                        const content = (
+                            <div
+                                className='problem-content'
+                                dangerouslySetInnerHTML={{
+                                    __html: problemContainer.innerHTML,
+                                }}
+                            ></div>
+                        );
+                        setProblemContent(content);
+                    } else {
+                        setProblemContent(
+                            <h1>문제를 불러오는데 실패했습니다.</h1>
+                        );
+                    }
+                },
+                (error) => {
+                    console.error('문제를 불러오는데 실패했습니다.', error);
                     setProblemContent(<h1>문제를 불러오는데 실패했습니다.</h1>);
                 }
-            } catch (error) {
-                console.error('문제를 불러오는데 실패했습니다.', error);
-                setProblemContent(<h1>문제를 불러오는데 실패했습니다.</h1>);
-            }
+            );
         };
 
         if (problemId) {
