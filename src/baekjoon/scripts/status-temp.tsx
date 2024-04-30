cfa0443709e9319c57e5fe695880e33cf33d9617
gusckd96 <gusckd96@naver.com>
2024-05-01 01:54:48 +0900

    Conflict: 충돌 문제 해결

--- src/baekjoon/scripts/status-temp.tsx
+++ src/baekjoon/scripts/status-temp.tsx
@@ -6,13 +6,17 @@ import {
 } from '@/baekjoon/utils/status';
 
 const customStatusPageTemp = () => {
+<<<<<<< HEAD
     console.log(
         getUrlSearchParam(window.location.href, 'after_algoplus_submit')
     );
+=======
+>>>>>>> 59fd25a (Feat: 문제 풀이 화면에서 제출 후 채점 상태 띄우기 구현)
     if (
         getUrlSearchParam(window.location.href, 'after_algoplus_submit') ===
         'true'
     ) {
+<<<<<<< HEAD
         console.log('채점 전');
         let timer = setInterval(() => {
             if (isJudgingState()) {
@@ -20,6 +24,12 @@ const customStatusPageTemp = () => {
             } else {
                 clearInterval(timer);
                 console.log(getFirstStatusResultTag());
+=======
+        let timer = setInterval(() => {
+            if (isJudgingState()) {
+            } else {
+                clearInterval(timer);
+>>>>>>> 59fd25a (Feat: 문제 풀이 화면에서 제출 후 채점 상태 띄우기 구현)
                 if (isWrongState()) {
                     alert(getFirstStatusResultTag());
                 }
