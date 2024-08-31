faefa9faca093dd1f0839759ae2b51e4b1d82717
양유경 <gosjorb@gmail.com>
2024-08-31 16:59:59 +0900

    Fix: Main 브랜치에서 잘못 덮어씌워진 파일 수정

--- src/baekjoon/containers/SolveView/SolveView.tsx
+++ src/baekjoon/containers/SolveView/SolveView.tsx
@@ -15,7 +15,6 @@ import EditorButtonBox from '@/baekjoon/presentations/EditorButtonBox/EditorButt
 import { LanguageSelectBox } from '@/baekjoon/components/LanguageSelectBox';
 import { CodeOpen, SubmitPostRequest } from '@/baekjoon/types/submit';
 import { submit } from '@/baekjoon/apis/submit';
-import { compile } from '@/common/apis/compile';
 import {
     convertLanguageIdForEditor,
     convertLanguageIdForReference,
@@ -153,49 +152,34 @@ const SolveView: React.FC<SolveViewProps> = ({
             testCase.result = undefined;
         });
 
-        if (testCases.length > 0) {
-            const data: CodeCompileRequest = {
-                lang: lang,
-                code: code,
-                input: testCases[0].input,
-            };
-
-            try {
-                const output = await compile(data);
-                if (checkCompileError(lang, output)) {
-                    setTestCaseState('error');
-                    setErrorMessage(output);
-                    return;
-                } else {
-                    testCases[0].result = output;
-                }
-            } catch (error) {
-                setTestCaseState('error');
-                setErrorMessage(
-                    `컴파일 서버에서 오류가 발생했습니다.\n${error}`
-                );
-                return;
-            }
-        }
-
-        await Promise.all(
-            currentTestCases.slice(1).map(async (testCase) => {
+        Promise.all(
+            currentTestCases.map(async (testCase, index) => {
                 const data: CodeCompileRequest = {
                     lang: lang,
                     code: code,
                     input: testCase.input,
                 };
 
-                try {
-                    const output = await compile(data);
-                    testCase.result = output; // 결과 설정
-                } catch (error) {
-                    setTestCaseState('error');
-                    setErrorMessage(
-                        `컴파일 서버에서 오류가 발생했습니다.\n${error}`
-                    );
-                    return;
-                }
+                chrome.runtime.sendMessage(
+                    { action: 'compile', data: data },
+                    (output) => {
+                        const newTestCases = [...currentTestCases];
+                        newTestCases[index].result = output;
+                        setTargetTestCases(newTestCases);
+                        if (checkCompileError(lang, output)) {
+                            setTestCaseState('error');
+                            setErrorMessage(output);
+                            return;
+                        }
+                        if (output == 'error') {
+                            setTestCaseState('error');
+                            setErrorMessage(
+                                `컴파일 서버에서 오류가 발생했습니다.\n`
+                            );
+                            return;
+                        }
+                    }
+                );
             })
         );
 
--- src/baekjoon/utils/compile.ts
+++ src/baekjoon/utils/compile.ts
@@ -2,15 +2,15 @@ import { CompilerLanguage } from '@/common/types/compile';
 
 const CompileErrorFormatConvertMap: Record<CompilerLanguage, string> = {
     c: 'prog.c:',
-    c_cpp: 'prog.cpp:',
+    cpp: 'prog.cpp:',
     csharp: './prog.cs',
     java: 'submission/Main.java',
-    python: 'File "./prog.py',
+    python3: 'File "./prog.py',
     nodejs: '/tmp/',
     kotlin: 'OpenJDK 64-Bit Server VM warning:',
     ruby: '# _/tmp/',
     swift: './prog.rb',
-    golang: '# _/tmp/',
+    go: '# _/tmp/',
 };
 
 const checkCompileError = (lang: CompilerLanguage, output: string): boolean => {
