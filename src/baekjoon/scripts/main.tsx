f9ea698e26b0d14a4acf140ffc633c4c95130c1c
hyeonchang <h-spear@10.nate.com>
2025-09-16 00:46:44 +0900

    Feat: SWEA 페이지 크롬 웹 스토어 manifest 설정 추가

---
+++ src/baekjoon/scripts/main.tsx
@@ -0,0 +1,23 @@
+import customProblemPage from '@/baekjoon/scripts/problem';
+import customStatusPage from '@/baekjoon/scripts/status';
+import customGlobalPage from '@/baekjoon/scripts/global';
+import customSubmitPage from '@/baekjoon/scripts/submit';
+
+const url: string = window.location.pathname;
+const searchUrl: string = window.location.search;
+
+export const customBaekjoonPage = () => {
+    if (
+        url.startsWith('/problem/') &&
+        !url.includes('status') &&
+        !url.includes('history')
+    ) {
+        customProblemPage();
+    } else if (url.startsWith('/status') && searchUrl.includes('from_mine')) {
+        customStatusPage();
+    } else if (url.startsWith('/submit')) {
+        customSubmitPage();
+    }
+
+    customGlobalPage();
+};
---
+++ src/swea/scripts/main.tsx
@@ -0,0 +1,6 @@
+const url: string = window.location.pathname;
+const searchUrl: string = window.location.search;
+
+export const customSweaPage = () => {
+    console.log('swea page');
+};
